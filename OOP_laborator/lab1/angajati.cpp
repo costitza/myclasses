6de708842c04291d4867b8b71e20d2ca1bae6d0b
Ababei Raul Costin <100480916+costitza@users.noreply.github.com>
2026-03-02 21:11:28 +0200

    fin

--- OOP_laborator/lab1/angajati.cpp
+++ OOP_laborator/lab1/angajati.cpp
@@ -14,6 +14,9 @@ private:
     int id;
 
 public:
+    Angajati(){
+        nume = nullptr;
+    }
     Angajati(char* _nume, double _salariu, int _id){
 
         id = _id;
@@ -25,13 +28,29 @@ public:
         strcpy(nume, _nume);
     }
 
+    Angajati& operator=(const Angajati& other){
+        delete[] nume;
+
+        id = other.id;
+        salariu = other.salariu;
+
+        nume = new char[strlen(other.nume) + 1];
+        strcpy(nume, other.nume);
+
+        return *this;
+    }
+
     ~Angajati(){
         delete[] nume;
     }
 
     // getter
-    double getSalariu(){
+    double getSalariu() const{
         return salariu;
+    }
+
+    char* getNume() const{
+        return nume;
     }
 
     // setter
@@ -39,8 +58,57 @@ public:
         nume = new char[strlen(sursa) + 1];
         strcpy(nume, sursa);
     };
+
+    void afis() const{
+
+        // setNume("salut");
+
+        cout << "nume: " << nume << ", salariu: " << salariu << ", id: " << id << '\n';
+    }
 };
 
+
+void salariuMare(Angajati* list, int n){
+
+    int maxim = -1;
+    int index = 0;
+    for (int i = 0; i < n;i ++){
+        if(maxim < list[i].getSalariu()){
+            maxim = list[i].getSalariu();
+            index = i;
+        }
+    }
+
+    cout << "\ncel mai smeker este: " << list[index].getNume() << '\n';
+    return;
+}
+
+
 int main(){
+    int n;
+    cin >> n;
+
+    Angajati* angajati = new Angajati[n];
+
+    char* nume = new char[255];
+    for (int i = 0; i < n; i++){
+        int id;
+        double salariu;
+        cin.ignore();
+        cin.getline(nume, 255);
+
+        //cin.get();
+        cin >> id;
+        cin >> salariu;
+
+        *(angajati + i) = Angajati(nume, salariu, id);
+    }
+
+    for (int i = 0; i < n; i++){
+        (angajati + i) -> afis();
+    }
+
+    salariuMare(angajati, n);
+
     return 0;
 }
