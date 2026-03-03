d07c0827a5d8d1661341b1a0ca48c959877cd6a3
Ababei Raul Costin <100480916+costitza@users.noreply.github.com>
2026-03-03 21:22:59 +0200

    intialized another branch

---
+++ SD_laborator/147.insertion-sort-list.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=147 lang=cpp
+ *
+ * [147] Insertion Sort List
+ */
+
+// @lc code=start
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        
+    }
+};
+// @lc code=end
+
--- SD_laborator/lab1/insertionsort.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by ababe on 2/27/2026.
-//
-
-
