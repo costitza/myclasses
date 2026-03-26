e78715674c63c8e5a5f70b3b60789aeba63a158d
Ababei Raul Costin <100480916+costitza@users.noreply.github.com>
2026-03-26 15:10:42 +0200

    problems but working on it

--- LFA_laborator/nfa/runner_nfa.py
+++ LFA_laborator/nfa/runner_nfa.py
@@ -1,40 +1,30 @@
 import sys
 import json
 
-
-
 def get_nfa(filepath):
-    with open('nfa.json', 'r') as f:
-        nfa = json.load(f)
-
+    with open(filepath, 'r') as f:
+        return json.load(f)
 
 def get_eps_closure(states, transitions):
-    
     closure = set(states)
     stack = list(states)
 
     while stack:
         curr_st = stack.pop()
-
         new_destinations = transitions.get(curr_st, {}).get("", [])
 
         for new_st in new_destinations:
             if new_st not in closure:
                 closure.add(new_st)
-
                 stack.append(new_st)
 
     return closure
 
-def run_nfa(nfa):
+def run_nfa(nfa, string):
     current_states = get_eps_closure(nfa['start_state'], nfa['transitions'])
 
-    string = sys.argv[1]
-
     for ch in string:
-
         next_states = set()
-
         for state in current_states:
             destinations = nfa['transitions'].get(state, {}).get(ch, [])
             next_states.update(destinations)
@@ -45,24 +35,25 @@ def run_nfa(nfa):
             break
     return current_states
 
-
-def validation(nfa, validation_states):
-    accept_sts = nfa['accept_states']
+def validation(nfa, validation_states, string):
+    accept_sts = set(nfa['accept_states'])
 
     if validation_states.intersection(accept_sts):
-        print(f"{sys.argv[1]} -> ACCEPTED")
+        print(f"{string} -> ACCEPTED")
     else:
-        print(f"{sys.argv[1]} -> REJECTED")
-
+        print(f"{string} -> REJECTED")
 
 def main():
-    nfa = get_nfa('nfa.json')
+    if len(sys.argv) < 3:
+        print("Usage: python runner_nfa.py <json_config_path> <input_string>")
+        return
 
-    results = run_nfa(nfa)
+    json_path = sys.argv[1]
+    input_string = sys.argv[2]
 
-    validation(nfa, results)
-
+    nfa = get_nfa(json_path)
+    results = run_nfa(nfa, input_string)
+    validation(nfa, results, input_string)
 
 if __name__ == "__main__":
     main()
-
